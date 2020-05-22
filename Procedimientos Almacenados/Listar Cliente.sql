f75dbc70393c028626cd85edf0a8bb26beeca05e
metal1snake <davidmandiola300@gmail.com>
2020-05-22 15:34:44 -0400

    se borró consultar cliente y se agrego listar cliente

---
+++ Procedimientos Almacenados/Listar Cliente.sql
@@ -0,0 +1,18 @@
+--------------------------------------------------------
+-- Archivo creado  - viernes-mayo-22-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Procedure LISTARCLIENTES
+--------------------------------------------------------
+set define off;
+
+  CREATE OR REPLACE PROCEDURE "PRUEBAS"."LISTARCLIENTES" (C_MEMORIA OUT SYS_REFCURSOR, P_RUNCLIENTE IN VARCHAR2)
+AS
+BEGIN 
+  OPEN C_MEMORIA FOR SELECT CLI.ID, DIR.PASAJE, DIR.NUMERO, DIR.COMUNA, CLI.RUNCLIENTE, CLI.DIGITOVERIF, CLI.ESTADO, CLI.NOMBRE, CLI.APELLIDO,
+                            CLI.CORREO, CLI.CONTRASENA FROM CLIENTE CLI
+                            INNER JOIN DIRECCION DIR ON CLI.IDDIRECCION = DIR.ID  
+                            WHERE RUNCLIENTE = P_RUNCLIENTE;
+END;
+
+/
